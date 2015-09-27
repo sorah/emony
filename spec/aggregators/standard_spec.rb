899daaf9923bd0561a7e3db8def9ad219cae5374
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-09-28 04:00:12 +0900

    Add aggregator spec

---
+++ spec/aggregators/standard_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'emony/aggregators/standard'
+
+describe Emony::Aggregators::Standard do
+  subject(:aggregator) { described_class.new(key: :n) }
+
+  it "calculates min/max/avg" do
+    aggregator.add n: 12
+    aggregator.add n: 18
+    aggregator.add n: 9
+
+    expect(aggregator.result[:average]).to eq 13
+    expect(aggregator.result[:max]).to eq 18
+    expect(aggregator.result[:min]).to eq 9
+    expect(aggregator.result[:count]).to eq 3
+    expect(aggregator.result[:total]).to eq 39
+  end
+
+  it "can accept merge" do
+    a = described_class.new(key: :n)
+    a.add(n: 12)
+    a.add(n: 18)
+    a.add(n: 9)
+
+    aggregator.add(n: 24)
+    aggregator.add(n: 3)
+    aggregator.add(n: 6)
+    aggregator.merge a.state
+
+    expect(aggregator.result[:average]).to eq 12
+    expect(aggregator.result[:max]).to eq 24
+    expect(aggregator.result[:min]).to eq 3
+    expect(aggregator.result[:count]).to eq 6
+    expect(aggregator.result[:total]).to eq 72
+  end
+end
