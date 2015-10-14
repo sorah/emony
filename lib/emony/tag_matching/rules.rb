9052797aa8b8454b9cf1925a3cff4ab870c882cc
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-15 00:41:41 +0900

    Accept symbols (because keys in config are Symbol)

--- lib/emony/tag_matching/rules.rb
+++ lib/emony/tag_matching/rules.rb
@@ -7,8 +7,9 @@ module Emony
   module TagMatching
     module Rules
       def self.create(str)
-        raise ArgumentError, 'str should be a String' unless str.is_a?(String)
+        raise ArgumentError, 'str should be a String or Symbol' unless str.is_a?(String) || str.is_a?(Symbol)
 
+        str = str.to_s
         case str
         when '*', '$default'
           Any.new(str)
