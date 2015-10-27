c2aa43a42fdc25b29f49c495a66650fa169812e6
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-27 23:02:29 +0900

    Add label on FinalizedWindow

--- lib/emony/finalized_window.rb
+++ lib/emony/finalized_window.rb
@@ -1,11 +1,11 @@
 module Emony
   class FinalizedWindow
     def self.from_window(window)
-      new(id: window.id, start: window.start, duration: window.duration, state: window.state, result: window.result)
+      new(label: window.label, id: window.id, start: window.start, duration: window.duration, state: window.state, result: window.result)
     end
 
-    def initialize(id: , start: , duration: , state: , result: )
-      @id, @start, @duration, @state, @result = id, start, duration, state, result
+    def initialize(label: , id: , start: , duration: , state: , result: )
+      @label, @id, @start, @duration, @state, @result = label, id, start, duration, state, result
     end
 
     def finish
@@ -16,6 +16,6 @@ module Emony
       self
     end
 
-    attr_reader :id, :start, :duration, :state, :result
+    attr_reader :label, :id, :start, :duration, :state, :result
   end
 end
