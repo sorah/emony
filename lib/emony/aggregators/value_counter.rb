0f1ca80bc0c7de0abd046c6b5536bc220bd4ac1a
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-10-31 11:46:52 +0900

    value_counter

---
+++ lib/emony/aggregators/value_counter.rb
@@ -0,0 +1,36 @@
+require 'emony/aggregators/base'
+module Emony
+  module Aggregators
+    class ValueCounter < Base
+      def initialize(*)
+        super
+
+        @data = Hash.new(0)
+      end
+
+      def result
+        @data.to_a.sort_by(&:first)
+      end
+
+      def state
+        @data
+      end
+
+      def key
+        @options[:key]
+      end
+
+      def aggregate(record)
+        val = record[key]
+        return unless val
+        @data[val] += 1
+      end
+
+      def aggregate_merge(state)
+        state.each do |k, v|
+          @data[k] += v
+        end
+      end
+    end
+  end
+end
