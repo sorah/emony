11c4bb08518b87131dab048bfd3bfa7faa3448c3
Shota Fukumori (sora_h) <sorah@cookpad.com>
2015-11-20 15:40:11 -0800

    Load all lib files for more accurate coverage
    only when it's going to run entire spec files (`bundle exec rspec`)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
+
+
 require 'simplecov'
 SimpleCov.start
 
 # require 'emony'
 
+if RSpec.configuration.instance_variable_get(:@files_or_directories_to_run) == %w(spec)
+  Dir["#{__dir__}/../lib/**/*.rb"].map do |x|
+    require x
+  end
+end
